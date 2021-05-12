2f851732b0804620774cf768bd990ab86517c692
Rupert Danton <khanov.ilya123@list.ru>
2021-05-12 16:23:18 +0300

    change headers' names from .h to .hpp

---
+++ Headers/run.hpp
@@ -0,0 +1,10 @@
+#ifndef __RUN_HPP__
+#define __RUN_HPP__
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Window.hpp>
+#include "un.hpp"
+
+int run(RenderWindow& window, unsigned int speed = 5);
+
+#endif //__RUN_HPP__
---
+++ Headers/un.hpp
@@ -0,0 +1,7 @@
+#ifndef __USING_NAMESPACES_HPP__
+#define __USING_NAMESPACES_HPP__
+
+using namespace sf;
+using namespace std;
+
+#endif //__USING_NAMESPACES_HPP__
